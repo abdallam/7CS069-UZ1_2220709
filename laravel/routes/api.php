522af8de788edd7820f9907716c444e72df0d86c
ipccproducts <36842131+abdallam@users.noreply.github.com>
2024-01-19 14:19:23 +0300

    API backend setup
    issuing tokens and authentication
    
    Co-Authored-By: Mudathir Abdallah <36842131+abdallam@users.noreply.github.com>

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/users/create', [Users::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/users/login', [Users::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::controller(Users::class)->group(function () {
+        Route::post('/user/{id}', 'show');
+        Route::get('/users', 'get');
+        Route::post('/user/update/{id}', 'update');
+        Route::post('/user/delete/{id}', 'delete');
+
+        Route::post('/users/logout', 'logout');
+    });
 });
